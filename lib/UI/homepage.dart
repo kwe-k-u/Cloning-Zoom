27f720ab2ca8a1fbaa47d274c4bd6fb82d33ab29
Kweku <47420306+kwe-k-u@users.noreply.github.com>
2020-07-29 13:16:15 +0000

    HomeScreen skeleton completed

--- lib/UI/homepage.dart
+++ lib/UI/homepage.dart
@@ -8,6 +8,121 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          title: Text("Meet & Chat"),
+          centerTitle: true,
+          leading: Icon(Icons.star_border),
+          actions: <Widget>[
+            Icon(Icons.launch)
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+
+              ),
+            ),
+                Divider(thickness: 1.5,),
+
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+
+                    Column(
+                      children: <Widget>[
+                        FlatButton(
+                          child: Icon(Icons.videocam),
+                          onPressed: (){
+                          },
+                          color: Colors.amberAccent,
+                        ),
+
+                        Text("New Meeting")
+                      ],
+                    ),
+
+                    Column(
+                      children: <Widget>[
+                        FlatButton(
+                          child: Icon(Icons.add_box),
+                          onPressed: (){
+                          },
+                          color: Colors.amberAccent,
+                        ),
+
+                        Text("Join")
+
+                      ],
+                    ),
+
+                    Column(
+                      children: <Widget>[
+
+                        FlatButton(
+                          child: Icon(Icons.calendar_today),
+                          onPressed: (){
+                          },
+                          color: Colors.amberAccent,
+                        ),
+                        Text("Share Screen")
+
+                      ],
+                    ),
+
+                    Column(
+                      children: <Widget>[
+                        FlatButton(
+                          child: Icon(Icons.open_in_browser),
+                          onPressed: (){
+                          },
+                          color: Colors.amberAccent,
+                        ),
+                        Text("Share Screen")
+                      ],
+                    ),
+
+                  ],
+                ),
+
+            Divider(thickness: 8,),
+
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text("Kweku Acquaye (you)"),
+                  ),
+
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text("Keith Asare"),
+                    subtitle: Text("Incoming call"),
+                    trailing: Text("Thur 28 May"),
+                  ),
+
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text("Michael Ofori"),
+                    subtitle: Text("Incoming Call"),
+                    trailing: Text("Thur 21 May"),
+                  ),
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
