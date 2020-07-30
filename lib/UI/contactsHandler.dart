7a6b9661f5889ace43826074bdf0b5b66fb9c174
Kweku <47420306+kwe-k-u@users.noreply.github.com>
2020-07-30 17:49:31 +0000

    Bottom tabs complete

---
+++ lib/UI/contactsHandler.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactsHandler extends StatefulWidget {
+  @override
+  _ContactsHandler createState() => _ContactsHandler();
+}
+
+class _ContactsHandler extends State<ContactsHandler> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_0720/UI/contactsHandler.dart';
 import 'package:zoom_0720/UI/homepage.dart';
 import 'package:zoom_0720/UI/joinMeeting.dart';
 import 'package:zoom_0720/UI/meetings.dart';
@@ -10,9 +11,65 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  int __currentIndex = 0;
+  List<Widget> pages = [
+
+  ];
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int __currentIndex = 0;
+  List<Widget> pages = [
+    Homepage(),
+    MeetingsPage(),
+    ContactsHandler(),
+    SettingsPage()
+  ];
+
+
   @override
   Widget build(BuildContext context) {
-    return ScheduleMeeting();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: pages[__currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: __currentIndex,
+            onTap: _displayPage,
+            selectedItemColor: Theme.of(context).accentColor,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.chat_bubble_outline),
+                  title: Text('Meet & Chat')
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.access_time),
+                  title: Text('Meetings')
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  title: Text('Contacts')
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  title: Text('Settings')
+              ),
+            ]),
+      ),
+    );
+  }
+
+  _displayPage(index){
+    setState(() {
+      __currentIndex = index;
+    });
   }
 }
+
